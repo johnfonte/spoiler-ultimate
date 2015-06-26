ff6b2259b8479f9d740649b35ce1e3b0c2d3d3c8
John Fonte <john@fonte.com>
2015-06-26 17:51:04 -0400

    schedule and links

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
-<html class="diagonal-gradient">
+<html class="">
 	<head>
 		<title>Spoiler Ultimate</title>
 		<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon"/>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<style type="text/css">
 			@font-face {
 			  font-family: 'Generica Bold';
@@ -16,15 +18,16 @@
 				font-family: 'DINBek-Regular';
 				color: orange;
 				height: 100%;
-				width: 100%;
+				background-color: black;
+				/*width: 100%;*/
 			}
 			a {
 				color: orange;
 			}
-			.wrapper {
+			/*.wrapper {
 				width: 100%;
 				height: 100%;
-			}
+			}*/
 			.diagonal-gradient {
 				background: #131313; /* Old browsers */
 				background: -moz-linear-gradient(-45deg, #131313 0%, #1c1c1c 12%, #2b2b2b 25%, #111111 39%); /* FF3.6+ */
@@ -65,11 +68,11 @@
 				position: absolute;
 				letter-spacing: 1px;
 			}
-			#tourney {
+			#tourney, #results {
 				width: 80%;
-				margin: 0 auto;
+				margin: 5px auto;
 			}
-			h1 {
+			h1, h2 {
 				color: orange;
 				margin: 0;
 				font-family: 'Generica Bold';
@@ -78,21 +81,54 @@
 			span.info {
 				display: block;
 			}
+			th {
+				font-weight: normal;
+				font-family: 'Generica Bold';
+				text-align: left;
+			}
+			table {
+				border-spacing: 5px;
+			}
+			@media (max-width: 760px) {
+				#splash {
+					/*width: 80%;*/
+					height: 150px;
+				}
+			}
 		</style>
 	</head>
 	<body>
-		<div class="diagonal-gradient"></div>
+		<!-- <div class="diagonal-gradient"></div> -->
 		<div class="wrapper">
 			<div id="header"><div id="logo"></div><span>Spoiler Ultimate</span></div>
 			<div id="splash"></div>
 			<div id="tourney">
 				<h1>SCINNY 2015</h1>
 				<div class="description">
-					SCINNY is our next tournament in Lebanon, Ohio on June 27th-28th.
-					<span class="info"><a href="https://www.google.com/maps/place/900+Mcclure+Rd,+Lebanon,+OH+45036/">Maps Link</a></span>
-					<span class="info"><a href="https://cincyultimate.org/tournament/scinny/2015/teams">Teams</a></span>
-					<span class="info"><a href="https://cincyultimate.org/tournament/scinny/2015">Tournament Info</a></span>
+					<span class="info">Lebanon, OH - June 27/28</span>
+					https://www.google.com/maps/place/Super+8+3553+Commerce+Drive+I-75+at+Exit+32+SR+122+Middletown,+OH+45005-5232+US/
+					<span class="info"><a href="https://www.google.com/maps/place/900+Mcclure+Rd,+Lebanon,+OH+45036/">Fields (Google Maps)</a></span>
+					<span class="info"><a href="http://scorereport.net/2015/open/event15150">Score Reporter</a></span>
+					<span class="info"><a href="https://cincyultimate.org/tournament/scinny/2015">Event Info (CUPA)</a></span>
 					<span class="info"><a href="https://twitter.com/SpoilerUltimate">Follow us on Twitter!</a></span>
+				</div>
+			</div>
+			<div id="results">
+				<div class="description">
+					<span class="info"><h2>Saturday</h2></span>
+					<table>
+						<thead>
+							<th>Time</th>
+							<th>Field</th>
+							<th>Opponent</th>
+						</thead>
+						<tbody>
+							<tr><td>9am</td><td>9</td><td>#7 HuSTLe</td></tr>
+							<tr><td>12pm</td><td>8</td><td>#9 CHASM</td></tr>
+							<tr><td>1:30pm</td><td>9</td><td>#5 Black Sheep</td></tr>
+							<tr><td>3pm</td><td>8</td><td>#1 Kentucky Flying Circus</td></tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 			<div id="roster"></div>
